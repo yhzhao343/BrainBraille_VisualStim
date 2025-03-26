fbc2b849739ce6d8ae078e0495118e7f61ef400a
Yuhui Zhao <yhzhao343@gmail.com>
2025-03-25 20:13:19 -0400

    change the port to use for dev and production

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
-export const WS_PORT = 8443;
+export const DEV_WS_PORT = 8443;
+export const PROD_WWS_PORT = 443;
 let WS_URL: string;
 if (!(window as any).IS_PRODUCTION) {
-  WS_URL = `ws://127.0.0.1:${WS_PORT}/BrainBraille_VisualStim/ws`;
+  WS_URL = `ws://127.0.0.1:${DEV_WS_PORT}/BrainBraille_VisualStim/ws`;
+  // WS_URL = `wss://yuhui-lab-server.duckdns.org:${WS_PORT}/BrainBraille_VisualStim/ws`;
 } else {
-  WS_URL = `wss://yuhui-lab-server.duckdns.org:${WS_PORT}/BrainBraille_VisualStim/ws`;
+  WS_URL = `wss://yuhui-lab-server.duckdns.org:${PROD_WWS_PORT}/BrainBraille_VisualStim/ws`;
 }
 
 export { WS_URL };
